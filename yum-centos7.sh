b5de253177d8c12f081c549ac24590e89666a748
Azril Reyes <azrilreyes@gmail.com>
2017-04-26 17:32:57 +0800

    Update yum-centos7.sh

--- yum-centos7.sh
+++ yum-centos7.sh
@@ -6,7 +6,7 @@ rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
 rpm --import https://rpms.remirepo.net/RPM-GPG-KEY-remi
 rpm -Uvh https://rpms.remirepo.net/enterprise/remi-release-7.rpm
 ###https://mariadb.orgr
-rpm -Uvh https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
+rpm --import https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
 cat >> /etc/yum.repos.d/mariadb.repo << 'EOF'
 # MariaDB 10.2 CentOS repository list
 # http://downloads.mariadb.org/mariadb/repositories/

bc5f6f72399848ab0b93f098d3532241c5a45408
Azril Reyes <azrilreyes@gmail.com>
2017-04-26 19:02:39 +0800

    Update addswap.sh

--- addswap.sh
+++ addswap.sh
@@ -11,3 +11,7 @@ fi
 
 echo "/swap none swap sw 0 0" >> /etc/fstab
 echo "done creating swapfile "
+echo "vm.swappiness = 10" | tee -a /etc/sysctl.conf
+echo "vm.vfs_pressure_cache = 50" tee -a /etc/sysctl.conf
+sysctl -p
+sysctl -a

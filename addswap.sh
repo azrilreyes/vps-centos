d79ad375c2676cff825007951cb753f4c9af6981
Azril Reyes <azrilreyes@gmail.com>
2017-04-26 19:04:53 +0800

    Update addswap.sh

--- addswap.sh
+++ addswap.sh
@@ -2,15 +2,13 @@
 if [ ! -e /swap ] ; then
     echo creating swap file for this machine
     fallocate -l 4G /swap && chmod 600 /swap && mkswap /swap && swapon /swap
+    echo "/swap none swap sw 0 0" >> /etc/fstab
     echo done creating swap file
 fi
-
+else 
 if [ ! -w "$file" ] ; then
     echo cannot write to $file
 fi
-
-echo "/swap none swap sw 0 0" >> /etc/fstab
-echo "done creating swapfile "
 echo "vm.swappiness = 10" | tee -a /etc/sysctl.conf
 echo "vm.vfs_pressure_cache = 50" tee -a /etc/sysctl.conf
 sysctl -p
